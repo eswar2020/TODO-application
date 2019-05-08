f56312140c6afd742677181958fb18fd09ae4c32
eswar2020 <mullapudieswar20@gmail.com>
2019-05-08 11:01:15 +0530

    todo.js

--- todo.js
+++ todo.js
@@ -24,8 +24,39 @@ var removeItems = document.getElementById('removeAll');
 removeItems.addEventListener('click',removeall)
 
 function addItem() {
-    console.log("add button clicked");
+    var input = document.getElementById('input');
+    var item = input.value;
+    ul = document.getElementById('list');
+    var textnode = document.createTextNode(item)
     
+    if (item === '') {
+        return false;
+    } else {
+        // create li
+        li =document.createElement('li');
+        
+        // create checkbox
+        var checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.setAttribute('id','check');
+
+        //create label
+        var label = document.createElement('label');
+
+        //add these things on web page
+        ul.appendChild(label);
+        li.appendChild(checkbox);
+        label.appendChild(textnode);
+        li.appendChild(label);
+        ul.insertBefore(li,ul.childNodes[0]);
+
+        setTimeout(() => {
+            li.className = 'visual'; 
+        }, 4);
+
+        input.value = '';
+        
+    }
 }
 function removeItem() {
     li = ul.children
@@ -42,8 +73,10 @@ function removeItem() {
 function removeall() {
     li = ul.children
     for (let index = 0; index < li.length; index++) {
-        ul.remove();
-    
+        while (li[index].children[0].checked) {
+            ul.removeChild(li[index]);
+        }
+        
         
         
     }
